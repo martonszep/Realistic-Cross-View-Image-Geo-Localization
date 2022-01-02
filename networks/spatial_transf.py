6e9f3be1ffb461ae93caced9b6c2cba269cfb91c
Marton Szep <marton.szep@tum.de>
2022-01-02 13:37:54 +0100

    extended composed spatial transf with residual blocks

--- networks/spatial_transf.py
+++ networks/spatial_transf.py
@@ -7,11 +7,12 @@ from data.cvusa_utils import convert_image_np
 
 # based on https://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html
 class SpatialTransf (nn.Module):
-    def __init__(self, in_channels, spatial_dims=None):
+    def __init__(self, in_channels, spatial_dims=None, sate_input=True):
         super(SpatialTransf, self).__init__()
 
         self._in_ch = in_channels 
         self.spatial_dims = spatial_dims
+        fc_size = 13*13 if sate_input==True else 4*36
 
         # Spatial transformer localization-network
         self.localization = nn.Sequential(
@@ -34,8 +35,8 @@ class SpatialTransf (nn.Module):
             
             # Regressor for the 3 * 2 affine matrix
             # Lambda(lambda x: print(x.size())),
-            Lambda(lambda x: x.view(-1, 10 * 13 * 13)), # calculated from satellite size 256x256, use the line above
-            nn.Linear(10 * 13 * 13, 64),
+            Lambda(lambda x: x.view(-1, 10 * fc_size)),
+            nn.Linear(10 * fc_size, 64),
             nn.ReLU(True),
             nn.Linear(64, 3 * 2) # 2D affine transf param
         )
@@ -59,19 +60,64 @@ class SpatialTransf (nn.Module):
         return x     
 
 class Lambda(nn.Module):
+    """
+    This is just a convenience function to make e.g. `nn.Sequential` more flexible.
+    """
     def __init__(self, func):
         super().__init__()
         self.func = func
 
     def forward(self, x):
-        return self.func(x) 
+        return self.func(x)
+
+class ResidualBlock(nn.Module):
+    """
+    The residual block used by ResNet.
+    
+    Args:
+        in_channels: The number of channels (feature maps) of the incoming embedding
+        out_channels: The number of channels after the first convolution
+        stride: Stride size of the first convolution, used for downsampling
+    """
+    
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()        
+        if stride > 1 or in_channels != out_channels:
+            # Add strides in the skip connection and zeros for the new channels.
+            self.skip = Lambda(lambda x: F.pad(x[:, :, ::stride, ::stride],
+                                               (0, 0, 0, 0, 0, out_channels - in_channels),
+                                               mode="constant", value=0))
+        else:
+            self.skip = nn.Sequential()
+            
+        # Initialize the required layers
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv1_bn = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2_bn = nn.BatchNorm2d(out_channels)
+        
+    def forward(self, input):
+        # Execute the required layers and functions
+        xb = F.relu(self.conv1_bn(self.conv1(input)))
+        z = self.conv2(xb) + self.skip(input)
+        xb = F.relu(self.conv2_bn(z))
+        return xb # [batch_size, num_classes] 
+
 
 class ComposedSpatialTransf (nn.Module):
-    def __init__(self, in_channels, spatial_dims):
+    def __init__(self, in_channels, spatial_dims, latent_channels=8):
         super(ComposedSpatialTransf, self).__init__()
 
+        # self.spatial_block = nn.Sequential(
+        #     SpatialTransf(in_channels, spatial_dims, sate_input=True)
+        # )
+
         self.spatial_block = nn.Sequential(
-            SpatialTransf(in_channels, spatial_dims)
+            SpatialTransf(in_channels, spatial_dims, sate_input=True),
+            ResidualBlock(in_channels, latent_channels),
+            SpatialTransf(latent_channels, spatial_dims, sate_input=False),
+            ResidualBlock(latent_channels, in_channels),
+            SpatialTransf(in_channels, spatial_dims, sate_input=False)
         )
 
     def forward(self, x):
