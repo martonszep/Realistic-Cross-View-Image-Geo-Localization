be3f9c7a823babdfa6a52d11ad280df27ce4bfa5
Marton Szep <marton.szep@tum.de>
2022-01-27 15:44:51 +0100

    changed spatial transformer to a modular structure

--- networks/spatial_transf.py
+++ networks/spatial_transf.py
@@ -1,20 +1,16 @@
+from turtle import forward
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-import matplotlib.pyplot as plt
-import torchvision
-from data.cvusa_utils import convert_image_np
 
-# based on https://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html
-class SpatialTransf (nn.Module):
-    def __init__(self, in_channels, spatial_dims=None, sate_input=True):
-        super(SpatialTransf, self).__init__()
+class AffineLocalization(nn.Module):
+    def __init__(self, in_channels, sate_input=True) -> None:
+        super(AffineLocalization, self).__init__()
 
-        self._in_ch = in_channels 
-        self.spatial_dims = spatial_dims
+        # Fully connected input size for images of size (256, 256) or (112, 616)
         fc_size = 13*13 if sate_input==True else 4*36
 
-        # Spatial transformer localization-network
+        # Affine spatial transformer localization-network
         self.localization = nn.Sequential(
             nn.Conv2d(in_channels, 8, kernel_size=7),
             nn.MaxPool2d(2, stride=2),
@@ -44,19 +40,38 @@ class SpatialTransf (nn.Module):
         # Initialize the weights/bias with identity transformation
         self.localization[-1].weight.data.zero_()        
         self.localization[-1].bias.data.copy_(torch.tensor([1, 0, 0, 
-                                                            0, 1, 0], dtype=torch.float)) # 2D affine transf param         
+                                                            0, 1, 0], dtype=torch.float)) # 2D affine transf param
+    def forward(self, x):
+        return self.localization(x)
+
+# based on https://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html
+class SpatialTransf (nn.Module):
+    def __init__(self, in_channels, spatial_dims=None, sate_input=True, tps=True):
+        super(SpatialTransf, self).__init__()
+
+        self._in_ch = in_channels 
+        self.spatial_dims = spatial_dims
+        self.tps = tps
+
+        if self.tps == True:
+            self.localization = None
+        else:
+            self.localization = AffineLocalization(in_channels=in_channels, sate_input=sate_input)
 
     def forward(self, x):
-        theta = self.localization(x)
-        theta = theta.view(-1, 2, 3) # 2D affine transf param
 
-        if self.spatial_dims is not None:
-            grid = F.affine_grid(theta, (x.size(0), self._in_ch, *self.spatial_dims), align_corners=False)
+        if self.tps == True:
+            pass
         else:
-            grid = F.affine_grid(theta, x.size(), align_corners=False)
+            theta = self.localization(x)
+            theta = theta.view(-1, 2, 3) # 2D affine transf param
+
+            if self.spatial_dims is not None:
+                grid = F.affine_grid(theta, (x.size(0), self._in_ch, *self.spatial_dims), align_corners=False)
+            else:
+                grid = F.affine_grid(theta, x.size(), align_corners=False)
         
         x = F.grid_sample(x, grid, mode='bilinear', align_corners=False)
-
         return x     
 
 class Lambda(nn.Module):
@@ -108,38 +123,19 @@ class ComposedSpatialTransf (nn.Module):
     def __init__(self, in_channels, spatial_dims, latent_channels=8):
         super(ComposedSpatialTransf, self).__init__()
 
+        self.use_tps = False
+
         # self.spatial_block = nn.Sequential(
-        #     SpatialTransf(in_channels, spatial_dims, sate_input=True)
+        #     SpatialTransf(in_channels, spatial_dims, sate_input=True, tps=self.use_tps)
         # )
 
         self.spatial_block = nn.Sequential(
-            SpatialTransf(in_channels, spatial_dims, sate_input=True),
+            SpatialTransf(in_channels, spatial_dims, sate_input=True, tps=self.use_tps),
             ResidualBlock(in_channels, latent_channels),
-            SpatialTransf(latent_channels, spatial_dims, sate_input=False),
+            SpatialTransf(latent_channels, spatial_dims, sate_input=False, tps=self.use_tps),
             ResidualBlock(latent_channels, in_channels),
-            SpatialTransf(in_channels, spatial_dims, sate_input=False)
+            SpatialTransf(in_channels, spatial_dims, sate_input=False, tps=self.use_tps)
         )
 
     def forward(self, x):
-        return self.spatial_block(x)  
-
-    # def visualize_stn(self, data):
-    #     with torch.no_grad():
-
-    #         input_tensor = data.cpu()
-    #         transformed_input_tensor = self.forward(data).cpu()
-
-    #         in_grid = convert_image_np(
-    #             torchvision.utils.make_grid(input_tensor))
-
-    #         out_grid = convert_image_np(
-    #             torchvision.utils.make_grid(transformed_input_tensor))
-
-    #         # Plot the results side-by-side
-    #         f, axarr = plt.subplots(1, 2)
-    #         axarr[0].imshow(in_grid)
-    #         axarr[0].set_title('Dataset Satellite Images')
-
-    #         axarr[1].imshow(out_grid)
-    #         axarr[1].set_title('Transformed Satellite Images')
-    #     return f
+        return self.spatial_block(x)
